0c99b3a03dd003b3902d66251fd2bd99ec783b29
mateocampo1004 <146900595+mateocampo1004@users.noreply.github.com>
2024-01-30 08:25:54 -0500

    a

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,53 +13,8 @@ import { FooterComponent } from './footer/footer.component';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent implements OnInit, OnDestroy{
+export class AppComponent{
   title = 'trabajo-en-clase';
-  menuVisible: boolean = false;
-
-  images = [
-    'assets/imagenes/1_carrusel_oferta_academica.jpg',
-    'assets/imagenes/2_carrusel_Inicial II.png',
-    'assets/imagenes/3_carrusel_Edu_general_Edu_general.png',
-    'assets/imagenes/4_carrusel_Bachi_ciencias.png',
-    'assets/imagenes/5_carrusel_bachi_informatico.png',
-    'assets/imagenes/6_carrusel_extracurriculares.png',
-    'assets/imagenes/7_carrusel_servicios_servicios.png',
-  ];
-
-  currentImageIndex = 0;
-  intervalId: any;
-
-  ngOnInit() {
-    this.startCarousel();
-  }
-
-  ngOnDestroy() {
-    if (this.intervalId) {
-      clearInterval(this.intervalId);
-    }
-  }
-
-  startCarousel() {
-    this.intervalId = setInterval(() => {
-      this.currentImageIndex = (this.currentImageIndex + 1) % this.images.length;
-    }, 4000); 
-  }
-  
-  prevSlide() {
-    if (this.currentImageIndex === 0) {
-      this.currentImageIndex = this.images.length - 1;
-    } else {
-      this.currentImageIndex--;
-    }
-  }
-
-  nextSlide() {
-    this.currentImageIndex = (this.currentImageIndex + 1) % this.images.length;
-  }
-
-  toggleMenu(): void {
-    this.menuVisible = !this.menuVisible;
-  }
+ 
 }
 
--- src/app/head/head.component.ts
+++ src/app/head/head.component.ts
@@ -10,7 +10,22 @@ import { Component } from '@angular/core';
 })
 export class HeadComponent {
   menuVisible = false;
+currentImageIndex: number=0;
+images: string[] = [   'assets/imagenes/1_carrusel_oferta_academica.jpg',
+'assets/imagenes/2_carrusel_Inicial II.png',
+'assets/imagenes/3_carrusel_Edu_general_Edu_general.png',
+'assets/imagenes/4_carrusel_Bachi_ciencias.png',
+'assets/imagenes/5_carrusel_bachi_informatico.png',
+'assets/imagenes/6_carrusel_extracurriculares.png',
+'assets/imagenes/7_carrusel_servicios_servicios.png',];
 
+prevSlide() {
+  this.currentImageIndex = (this.currentImageIndex - 1 + this.images.length) % this.images.length;
+}
+
+nextSlide() {
+  this.currentImageIndex = (this.currentImageIndex + 1) % this.images.length;
+}
   toggleMenu() {
     this.menuVisible = !this.menuVisible;
   }
